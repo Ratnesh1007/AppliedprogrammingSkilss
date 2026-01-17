181d8bba7be25951763d9cb6d7ae71764760aed1
Ratnesh1007 <vtu24403@veltech.edu.in>
2026-01-18 01:19:06 +0530

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,25 @@
+class Task4 {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int totalSum = 0;
+        for (int x : nums) {
+            totalSum += x;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+
+            result[i] =
+                    nums[i] * i - leftSum
+                  + rightSum - nums[i] * (n - i - 1);
+
+            leftSum += nums[i];
+        }
+
+        return result;
+    }
+}
---
+++ Task7.java
@@ -0,0 +1,15 @@
+class Task7 {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;       // best day to buy
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
